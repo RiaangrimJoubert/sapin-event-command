98c71b2b36bb5cc6bfdc710c6a1022697bfebe28
RiaangrimJoubert <riaangrimjoubert@gmail.com>
2026-04-23 22:02:08 +0200

    Create Partners.jsx

---
+++ src/pages/Partners.jsx
@@ -0,0 +1,10 @@
+export default function Partners() {
+  return (
+    <div className="page-placeholder">
+      <h1 className="page-placeholder-title">Partners</h1>
+      <p className="page-placeholder-copy">
+        Partners layout coming next.
+      </p>
+    </div>
+  );
+}
