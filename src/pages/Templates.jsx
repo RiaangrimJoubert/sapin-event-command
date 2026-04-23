ae13b273f093916d86018b06e004c65dfba8b64d
RiaangrimJoubert <riaangrimjoubert@gmail.com>
2026-04-23 22:04:58 +0200

    Create Templates.jsx

---
+++ src/pages/Templates.jsx
@@ -0,0 +1,10 @@
+export default function Templates() {
+  return (
+    <div className="page-placeholder">
+      <h1 className="page-placeholder-title">Templates</h1>
+      <p className="page-placeholder-copy">
+        Templates layout coming next.
+      </p>
+    </div>
+  );
+}
