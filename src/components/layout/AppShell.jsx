7fc4d8d48b17432bcad28162cd7f46ee5d4d9f28
RiaangrimJoubert <riaangrimjoubert@gmail.com>
2026-04-23 21:54:47 +0200

    Update AppShell.jsx

--- src/components/layout/AppShell.jsx
+++ src/components/layout/AppShell.jsx
@@ -1,12 +1,12 @@
 import Sidebar from "./Sidebar";
 import Topbar from "./Topbar";
 
-export default function AppShell({ children }) {
+export default function AppShell({ children, activeView, onNavigate }) {
   return (
     <div className="app-bg">
       <div className="app-frame">
         <div className="app-grid">
-          <Sidebar />
+          <Sidebar activeView={activeView} onNavigate={onNavigate} />
           <main className="main-panel">
             <Topbar />
             {children}
