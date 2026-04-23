0fc4f8dca20263a1a993ba2662b4b0909b9542e3
RiaangrimJoubert <riaangrimjoubert@gmail.com>
2026-04-23 21:54:22 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
 import AppShell from "./components/layout/AppShell";
 import Dashboard from "./pages/Dashboard";
+import Events from "./pages/Events";
 
 export default function App() {
+  const [activeView, setActiveView] = useState("dashboard");
+
+  const renderView = () => {
+    switch (activeView) {
+      case "events":
+        return <Events />;
+      case "dashboard":
+      default:
+        return <Dashboard />;
+    }
+  };
+
   return (
-    <AppShell>
-      <Dashboard />
+    <AppShell activeView={activeView} onNavigate={setActiveView}>
+      {renderView()}
     </AppShell>
   );
 }
